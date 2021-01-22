19d41ba8ecbdc78c41b6868cd788d9125a8c5a99
Alex Sircu <alex.sricu@gmail.com>
2021-01-22 17:27:03 +0100

    main con php e scss

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <?php
-    include __DIR__ . '/partials/db.php';
- ?>
+include __DIR__ . '/partials/db.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/app.css">
     <title>Document</title>
 </head>
+
 <body>
     <header></header>
 
     <main>
         <div class="container">
-            <?php foreach ($discs as $oneDisc) {?>
+            <?php foreach ($discs as $oneDisc) { ?>
                 <div class="disc-container">
-                    <img src="" alt="">
+                    <img src="<?php echo $oneDisc["poster"]; ?>" alt="">
                     <h2><?php echo $oneDisc["title"]; ?></h2>
                     <h3><?php echo $oneDisc["author"]; ?></h3>
-                    <h4><?php echo $oneDisc["year"]; ?></h4>
+                    <p><?php echo $oneDisc["year"]; ?></p>
                 </div>
             <?php } ?>
         </div>
@@ -28,4 +30,5 @@
 
     <footer></footer>
 </body>
+
 </html>
